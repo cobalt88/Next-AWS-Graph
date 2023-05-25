173ba2146ab48cb0c533dbb80f6ed25a144104b0
Cobalt88 <vincentteune@gmail.com>
2023-05-25 15:18:32 -0400

    kill the navbar

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/PageSections/Footer.jsx";
 import Head from "next/head";
-import NavBar from "@/components/Navigation/NavBar.jsx";
+
 import Header from "@/components/PageSections/Header.jsx";
 
 export default function MainLayout(props) {
@@ -13,7 +13,7 @@ export default function MainLayout(props) {
 				<meta name="author" content={props.author} />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
-			{/* <NavBar navProps={props.navigation} /> */}
+
 			<main>{props.children}</main>
 
 			<Footer />
